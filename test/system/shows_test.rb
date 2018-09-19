7ec5bc8cc0261a73e2ffcd6e943815402ec58144
Henry Haller <henrybhaller@gmail.com>
2018-09-19 09:36:18 -0400

    basic show life cycle testing

--- test/system/shows_test.rb
+++ test/system/shows_test.rb
@@ -5,4 +5,48 @@ class ShowsTest < ApplicationSystemTestCase
     visit "/"
     assert_selector "h4", text: "RSSHub Home"
   end
+
+
+  def login(user)
+    login_as users(user)
+    visit episodes_path
+    assert_equal episodes_path, page.current_path
+  end
+
+  test "lets a new user create and delete a new show" do
+    login(:george)
+    # save_and_open_screenshot
+
+    fill_in "show_rss_url", with: "http://localhost:3001/feed.rss"
+    # save_and_open_screenshot
+
+    click_on 'Create Show'
+    # save_and_open_screenshot
+
+    fill_in "show_rss_url", with: "http://localhost:3001/feed.rss"
+    # save_and_open_screenshot
+
+    click_on 'Create Show'
+    # save_and_open_screenshot
+    assert_text "Test Feed", maximum: 1
+
+    # Should be redirected to Home with new product
+    assert_text "Episodes"
+    click_on 'Delete'
+
+    assert_text "Test Feed", maximum: 0
+    assert_equal episodes_path, page.current_path
+
+    # save_and_open_screenshot
+  end
+
+  # test "lets a new user delete their new show" do
+  #   login(:george)
+
+  #   click_on 'Delete'
+
+  #   assert_text "Test Feed", maximum: 0
+  #   assert_equal episodes_path, page.current_path
+  # end
+
 end
