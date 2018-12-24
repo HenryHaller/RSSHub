761761c17912616722807709a170246b1ff0dc5f
Henry Haller <henrybhaller@gmail.com>
2018-12-24 14:30:50 +0700

    user can get urls they are subscribed to

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   scope :admin do
     get 'panel', to: 'admin#panel'
   end
+  get 'urls', to: 'shows#urls'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
