536d7383948cc6e7f32755618ad7ba639af819b2
Henry Haller <henrybhaller@gmail.com>
2018-12-26 16:28:33 +0700

    big formatting update

---
+++ app/javascript/packs/resize_management_height.js
@@ -0,0 +1,6 @@
+const episodeColumn = document.querySelector("#episode-column");
+const management = document.querySelector("#manage-shows");
+
+const height = episodeColumn.offsetHeight;
+management.style.height = `${height}px`;
+
