72a8618a16d515aedeaceaa1a4d8ad8dd9130d3a
Henry Haller <henrybhaller@gmail.com>
2019-01-06 22:56:28 +0700

    large javascript update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require bootstrap/dist/js/bootstrap
+//= require responsive-toolkit/dist/bootstrap-toolkit
 //= require_tree .
--- app/javascript/packs/resize_management_height.js
+++ app/javascript/packs/resize_management_height.js
@@ -1,6 +1,36 @@
-const episodeColumn = document.querySelector("#episode-column");
-const management = document.querySelector("#manage-shows");
+// https://github.com/maciej-gurban/responsive-bootstrap-toolkit#basic-usage
+// Wrap IIFE around your code
+(function($, viewport){
+  $(document).ready(function() {
+    const episodeColumn = document.querySelector("#episode-column");
+    const management = document.querySelector("#manage-shows");
 
-const height = episodeColumn.offsetHeight;
-management.style.height = `${height}px`;
+    const setEqualHeight = () => {
+      const height = episodeColumn.offsetHeight;
+      management.style.height = `${height}px`;
+    }
+    const setIndependantHeight = () => {
+      management.style.height = 'auto';
+    }
 
+
+    if(viewport.is('<=sm')) {
+      setIndependantHeight();
+    }
+    else {
+      setEqualHeight();
+    }
+
+
+    // Execute code each time window size changes
+    $(window).resize(
+      viewport.changed(function() {
+        if(viewport.is('<=sm')) {
+          setIndependantHeight();        }
+        else {
+          setEqualHeight();
+        }
+      }, 150) // custom debounce interval
+      );
+  });
+})(jQuery, ResponsiveBootstrapToolkit);
