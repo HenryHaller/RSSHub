2de2975ebe30131df75c96ecbd3c4faa503992a1
Henry Haller <henrybhaller@gmail.com>
2019-01-06 15:55:29 +0700

    worked on adding download option and tweaked padding

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,14 @@
 class Episode < ApplicationRecord
   belongs_to :show
   validates :title, presence: true, uniqueness: { scope: :show }
+  def safe_title
+    t = title
+    t.tr!("'", "")
+    t.tr!('"', "")
+    t.tr!('(', '')
+    t.tr!(')', '')
+    t.tr!('/', ' ')
+    t.tr!(' ', '_')
+    t
+  end
 end
