442a0644f55bc1c7b972686cf2a4714defbcde81
Henry Haller <henrybhaller@gmail.com>
2019-01-13 23:19:48 +0700

    some formatting changes

--- app/javascript/packs/resize_title.js
+++ app/javascript/packs/resize_title.js
@@ -1,9 +1,14 @@
-const target = document.querySelector("#episodes");
-const title = document.querySelector("#episodes-title");
+const episodes = document.querySelector("#episodes");
+const management = document.querySelector("#manage-shows");
+const episodesTitle = document.querySelector("#episodes-title");
+const managementTitle = document.querySelector("#management-title");
 
 const reSize = () => {
-  let width = target.offsetWidth;
-  title.style.width = `${width}px`;
+  let width = episodes.offsetWidth;
+  episodesTitle.style.width = `${width}px`;
+  managementWidth = management.offsetWidth;
+  managementTitle.style.width = `${managementWidth}px`;
+
 }
 
 reSize();
