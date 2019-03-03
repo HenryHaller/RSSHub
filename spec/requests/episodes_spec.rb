5c23664cc7d6991a13012ec57e87d39785c3907c
Henry Haller <henrybhaller@gmail.com>
2019-03-04 02:01:29 +0900

    add a test for show title property

--- spec/requests/episodes_spec.rb
+++ spec/requests/episodes_spec.rb
@@ -6,19 +6,26 @@ RSpec.describe 'Episodes API' do
   let!(:episodes) { create_list(:episode, 5, show: show)}
   let(:show_id) { show.id }
   let(:headers) { valid_headers }
-
-
+  
+  
   describe "GET /episodes" do
     before { get "/episodes/", headers: headers }
-
+    
     context "We Can Get Episodes" do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
-
+      
       it 'returns 5 episodes' do
         expect(json.size).to eq(5)
+      end
+      
+      it 'every episode has a show title' do
+        json.each do |episode|
+          expect(episode["show-title"]).to be_a(String)
+        end
       end
     end
   end
 end
+    
