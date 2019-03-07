9c85ee619e51d7a7542592eae649a109a9fa9b6c
Henry Haller <henrybhaller@gmail.com>
2019-03-07 15:27:15 +0900

    update frontend url in mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,12 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: ENV["DOMAIN"] }
   config.action_mailer.delivery_method = :sendgrid_actionmailer
   config.action_mailer.sendgrid_actionmailer_settings = {
     api_key: ENV['SENDGRID_API_KEY'],
     raise_delivery_errors: true
   }
+
+  config.action_mailer.default_url_options = { host: ENV["FRONTEND_URL"] }
+
 
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
