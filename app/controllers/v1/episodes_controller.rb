4fb143376049624e77be3df808d8860084833a97
Henry Haller <henrybhaller@gmail.com>
2019-03-16 22:59:10 +0900

    fix n+1 problem

--- app/controllers/v1/episodes_controller.rb
+++ app/controllers/v1/episodes_controller.rb
@@ -1,7 +1,7 @@
 module V1
   class EpisodesController < ApplicationController
     def index
-      @episodes = current_user.episodes.order(pub_date: :desc).first(15)
+      @episodes = current_user.episodes.includes(:show).order(pub_date: :desc).first(15)
       json_response(@episodes)
     end
   end
