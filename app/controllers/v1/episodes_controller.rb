064034ffc7f3d2bddf5be1a9c16ff61d258749e9
Henry Haller <henrybhaller@gmail.com>
2019-03-25 16:28:43 +0900

    make episodes cache private

--- app/controllers/v1/episodes_controller.rb
+++ app/controllers/v1/episodes_controller.rb
@@ -1,7 +1,7 @@
 module V1
   class EpisodesController < ApplicationController
     def index
-      if stale?(etag: current_user, last_modified: current_user.updated_at, public: true)
+      if stale?(etag: current_user, last_modified: current_user.updated_at, public: false)
         @episodes = current_user.episodes.includes(:show).order(pub_date: :desc).first(15)
         json_response(@episodes)
       end
